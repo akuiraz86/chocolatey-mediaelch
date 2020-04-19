b084833e9ac38acafc635842341ead8e6e141461
akuiraz86 <47557907+akuiraz86@users.noreply.github.com>
2020-04-18 22:34:16 -0400

    new vwre

--- src/tools/chocolateyInstall.ps1
+++ src/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@ $ErrorActionPreference = 'Stop'
 
 $packageArgs = @{
   packageName    = 'MediaElch'
-  url            = 'https://github.com/Komet/MediaElch/releases/download/v2.6.4/MediaElch_2.6.4_win.zip'
-  checksum       = '6279625A8E0541E1B411F9DB0C55B70ED42DBA15009E729CFAA12FF1B4983ACA'
+  url            = 'https://dl.bintray.com/komet/MediaElch/:MediaElch_win_2.6.7-dev_2020-04-18_17-35_git-master-1d231b0.zip'
+  checksum       = '564DF01104CB8AA8C14B45CEABABE137CB05B6EAA27D17F15EAB358977AA00C1'
   checksumType   = 'sha256'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 }
